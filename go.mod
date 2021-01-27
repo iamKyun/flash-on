a3e971f9e1725b39bc98f20a7597000773f91e40
Kyun <kyun@iamkyun.com>
2021-01-27 10:10:54 +0800

    v0.3

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module iamkyun.com/flash-on
 
 go 1.15
+
+require github.com/mattn/go-colorable v0.1.8
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/mattn/go-colorable"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -20,48 +21,64 @@ func main() {
 	} else {
 		filePath = "C:\\Windows\\System32\\Macromed\\Flash\\Flash.ocx"
 	}
-	fmt.Println("Flash On v0.3")
-	fmt.Println("！！！运行前请关闭浏览器！！！")
+	str :=
+		` ---------------
+| Flash On v0.3 |
+ ---------------
+
+`
+	fmt.Print(str)
+
+	fmt.Printf("！！！运行前请关闭浏览器！！！\n\n")
 	fmt.Printf("%d位操作系统\n", bit)
-	fmt.Printf("读取文件：%s\n", filePath)
 	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		fmt.Println("读取文件失败，请检查文件是否存在")
+		PrintRed("\n读取文件失败，请检查文件是否存在\n")
 		return
 	}
 	targetBytes := []byte{0x00, 0x00, 0x40, 0x46, 0x3E, 0x6F, 0x77}
-	fmt.Println("开始查找目标代码...")
 	if bytes.Contains(content, targetBytes) {
 		targetIndex := bytes.Index(content, targetBytes)
 		modifyIndex := targetIndex + 7
 		if content[modifyIndex] == 0x72 {
-			fmt.Println("已被激活的Flash，不需要操作")
+			PrintGreen("\n已被激活的Flash，不需要操作\n")
 		} else {
-			fmt.Println("查找到了目标代码")
-			fmt.Println("开始尝试修改...")
-			fmt.Println("获取文件权限...")
+			fmt.Println("开始尝试激活...")
 			c := exec.Command("cmd", "/C", "TAKEOWN", "/F", filePath, "/a")
 			if err := c.Run(); err != nil {
-				fmt.Println("修改文件权限失败，请用管理员权限运行！")
+				PrintRed("获取文件权限失败，请用管理员权限运行！\n")
 				return
 			}
 			c2 := exec.Command("cmd", "/C", "ICACLS", filePath, "/grant", "Administrators:F")
 			if err := c2.Run(); err != nil {
-				fmt.Println("修改文件权限失败，请用管理员权限运行！")
+				PrintRed("获取文件权限失败，请用管理员权限运行！\n")
 				return
 			}
 			content[modifyIndex] = 0x72
 			err := ioutil.WriteFile(filePath, content, os.ModePerm)
 			if err == nil {
-				fmt.Println("修改成功，flash已被激活")
+				PrintGreen("修改成功，flash已被激活\n")
 			} else {
-				fmt.Println("修改失败，原因：")
+				PrintRed("修改失败，原因：\n")
 				fmt.Println(err)
 			}
 		}
 	} else {
-		fmt.Println("找不到目标代码，修改失败")
+		PrintRed("找不到目标代码，修改失败")
 	}
 	fmt.Print("\n按enter键退出...")
 	fmt.Scanln()
+}
+
+func PrintRed(str string) {
+	PrintColor(str, "31")
+}
+
+func PrintGreen(str string) {
+	PrintColor(str, "32")
+}
+
+func PrintColor(str string, color string) {
+	stdout := colorable.NewColorable(os.Stdout)
+	_, _ = fmt.Fprintf(stdout, "\x1b["+color+"m%s\x1b[0m", str)
 }
